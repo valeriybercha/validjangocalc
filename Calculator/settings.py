eb709b2d4f2b76337592579b4f40263a2ab75c50
Valeriy Bercha <valber.motorsport@gmail.com>
2020-04-13 14:04:05 +0300

    minor changes to settings.py file

--- Calculator/settings.py
+++ Calculator/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5!pu2bqdavjo+fn&00(&)*gs)t#uav-0bc*7ur_ie-q-po!!2l'
+SECRET_KEY = '<your secret key here>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
